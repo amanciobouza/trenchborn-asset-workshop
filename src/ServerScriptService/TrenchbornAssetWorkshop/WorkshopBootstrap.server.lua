127a1299455885da641b9e0c9ca9fced707bc895
Amancio Bouza <amancio.bouza@gmail.com>
2026-08-30 10:58:55 +0200

    Apply Roadblock dressing in workshop

--- src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
+++ src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
@@ -3,16 +3,18 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local workshop = Workspace:WaitForChild("TrenchbornAssetWorkshop")
 workshop:SetAttribute("Pipeline", "Trenchborn-7-Phase")
-workshop:SetAttribute("QualityStatus", "Phase4_GoldenMaster")
+workshop:SetAttribute("QualityStatus", "Phase5_RobloxDressing")
 
 local packageFolder = ReplicatedStorage:WaitForChild("TrenchbornAssetWorkshop")
 local specification = require(packageFolder:WaitForChild("MarshalRoadblockSpecification"))
 local goldenMaster = require(packageFolder:WaitForChild("MarshalRoadblockGoldenMaster"))
+local dressing = require(packageFolder:WaitForChild("MarshalRoadblockDressing"))
 local wardenGoldenMaster = require(packageFolder:WaitForChild("WardenShepherdGoldenMaster"))
 
 workshop:SetAttribute("CurrentAsset", specification.AssetName)
 workshop:SetAttribute("CurrentPhase", specification.PipelinePhase)
 local model = goldenMaster.Build(workshop)
+dressing.Apply(model)
 model:PivotTo(model:GetPivot() + Vector3.new(-15, 0, 0))
 
 local comparisonWarden = wardenGoldenMaster.Build(workshop)
