fc951decb2056f01359cbaa52776e0f03353eb38
Amancio Bouza <amancio.bouza@gmail.com>
2026-08-30 11:06:36 +0200

    Activate Roadblock phase 6 simulation

--- src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
+++ src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
@@ -3,12 +3,15 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local workshop = Workspace:WaitForChild("TrenchbornAssetWorkshop")
 workshop:SetAttribute("Pipeline", "Trenchborn-7-Phase")
-workshop:SetAttribute("QualityStatus", "Phase5_RobloxDressing")
+workshop:SetAttribute("QualityStatus", "Phase6_GameplaySimulation")
 
 local packageFolder = ReplicatedStorage:WaitForChild("TrenchbornAssetWorkshop")
 local specification = require(packageFolder:WaitForChild("MarshalRoadblockSpecification"))
 local goldenMaster = require(packageFolder:WaitForChild("MarshalRoadblockGoldenMaster"))
 local dressing = require(packageFolder:WaitForChild("MarshalRoadblockDressing"))
+local gameplayConfig = require(packageFolder:WaitForChild("MarshalRoadblockGameplayConfig"))
+local gameplay = require(packageFolder:WaitForChild("MarshalRoadblockGameplay"))
+local testHarness = require(packageFolder:WaitForChild("MarshalRoadblockTestHarness"))
 local wardenGoldenMaster = require(packageFolder:WaitForChild("WardenShepherdGoldenMaster"))
 
 workshop:SetAttribute("CurrentAsset", specification.AssetName)
@@ -16,6 +19,8 @@ workshop:SetAttribute("CurrentPhase", specification.PipelinePhase)
 local model = goldenMaster.Build(workshop)
 dressing.Apply(model)
 model:PivotTo(model:GetPivot() + Vector3.new(-15, 0, 0))
+gameplay.Attach(model, gameplayConfig)
+testHarness.Attach(workshop, model, gameplayConfig)
 
 local comparisonWarden = wardenGoldenMaster.Build(workshop)
 comparisonWarden.Name = "Warden_I_Shepherd_Comparison"
