c8cce2506f3b0f8beb9f22f970dfd91c5247ade6
Amancio Bouza <amancio.bouza@gmail.com>
2026-08-30 10:17:22 +0200

    Show Marshal-II Roadblock golden master in workshop

--- src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
+++ src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
@@ -3,16 +3,12 @@ local ReplicatedStorage = game:GetService("ReplicatedStorage")
 
 local workshop = Workspace:WaitForChild("TrenchbornAssetWorkshop")
 workshop:SetAttribute("Pipeline", "Trenchborn-7-Phase")
-workshop:SetAttribute("QualityStatus", "Phase7_FinalInstaller")
+workshop:SetAttribute("QualityStatus", "Phase4_GoldenMaster")
 
 local packageFolder = ReplicatedStorage:WaitForChild("TrenchbornAssetWorkshop")
-local specification = require(packageFolder:WaitForChild("AssetSpecification"))
-local installer = require(packageFolder:WaitForChild("WardenShepherdInstaller"))
-local testHarness = require(packageFolder:WaitForChild("WardenShepherdTestHarness"))
+local specification = require(packageFolder:WaitForChild("MarshalRoadblockSpecification"))
+local goldenMaster = require(packageFolder:WaitForChild("MarshalRoadblockGoldenMaster"))
 
 workshop:SetAttribute("CurrentAsset", specification.AssetName)
 workshop:SetAttribute("CurrentPhase", specification.PipelinePhase)
-local model = installer.Install(workshop, {
-	GroundCFrame = CFrame.new(0, 0, 0),
-})
-testHarness.Attach(workshop, model)
+local model = goldenMaster.Build(workshop)
