c67df7713042f8575c9f6fb26b95084965c5e54e
Amancio Bouza <amancio.bouza@gmail.com>
2026-08-30 10:55:10 +0200

    Add Warden-I comparison beside Roadblock golden master

--- src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
+++ src/ServerScriptService/TrenchbornAssetWorkshop/WorkshopBootstrap.server.lua
@@ -8,7 +8,14 @@ workshop:SetAttribute("QualityStatus", "Phase4_GoldenMaster")
 local packageFolder = ReplicatedStorage:WaitForChild("TrenchbornAssetWorkshop")
 local specification = require(packageFolder:WaitForChild("MarshalRoadblockSpecification"))
 local goldenMaster = require(packageFolder:WaitForChild("MarshalRoadblockGoldenMaster"))
+local wardenGoldenMaster = require(packageFolder:WaitForChild("WardenShepherdGoldenMaster"))
 
 workshop:SetAttribute("CurrentAsset", specification.AssetName)
 workshop:SetAttribute("CurrentPhase", specification.PipelinePhase)
 local model = goldenMaster.Build(workshop)
+model:PivotTo(model:GetPivot() + Vector3.new(-15, 0, 0))
+
+local comparisonWarden = wardenGoldenMaster.Build(workshop)
+comparisonWarden.Name = "Warden_I_Shepherd_Comparison"
+comparisonWarden:SetAttribute("ComparisonReference", true)
+comparisonWarden:PivotTo(comparisonWarden:GetPivot() + Vector3.new(16, 0, 0))
