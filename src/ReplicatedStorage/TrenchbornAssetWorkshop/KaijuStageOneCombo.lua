cd4c698870ef702cdebb87edb731d4a56630ceab
Amancio Bouza <amancio.bouza@gmail.com>
2026-09-13 01:38:02 +0200

    Advance tear-apart sound by another 150ms

--- src/ReplicatedStorage/TrenchbornAssetWorkshop/KaijuStageOneCombo.lua
+++ src/ReplicatedStorage/TrenchbornAssetWorkshop/KaijuStageOneCombo.lua
@@ -106,7 +106,7 @@ function Combo.new(prepareFinisher)
 			self.GrabSent = true
 			table.insert(self.Events, {Kind="Grab", Index=4})
 		end
-		if self.Index==4 and t>=impactTimes[4]-0.18 and not self.TearSoundSent then
+		if self.Index==4 and t>=impactTimes[4]-0.33 and not self.TearSoundSent then
 			self.TearSoundSent=true
 			table.insert(self.Events,{Kind="TearSound",Index=4})
 		end
